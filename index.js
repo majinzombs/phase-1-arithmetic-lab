a9b13565e07a61f95e372fb033cdcbe5e39de097
Jonathan Koban <jon.koban@outlook.com>
2022-04-10 12:05:31 -0400

    commit tree 1b880b66cb34b905640c085bee3e89c99307da7c

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+const multiply = (num1 * num2)
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 22
+let num4 = 6
+const mod = Math.ceil((num3 / num4));
+
+const max = Math.max(2,5,9,14,16,20)
